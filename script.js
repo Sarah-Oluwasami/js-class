517142241ecd72d5d707e8ccba506a5ed2a4260f
Sarah Oluwasami <saraholuwasami43@gmail.com>
2022-09-26 02:02:33 +0100

    third experience

---
+++ script.js
@@ -0,0 +1,57 @@
+
+let sum = "OIC ";
+sum += "Hub ";
+sum += "Nigeria";
+
+let substr = 20;
+substr -= 4;
+substr -= 5;
+
+let modul = 25;
+modul %= 4;
+modul %= 3;
+
+let divide = 20;
+divide /= 4;
+divide /= 4;
+
+let multiply = 6;
+multiply *= 4;
+multiply *= 5;
+
+let expo = 2;
+expo **= 2;
+expo **= 3;
+
+
+
+document.getElementById('suboperator').innerHTML="Assignment concatenation Operator";
+document.getElementById('ans').innerHTML="<code>let Sum=\"OIC\"</code> <br/> <code> Sum +=\"Hub\"</code><br/> <code> Sum +=\"Nigeria\"</code><br/>" +  sum;
+
+document.getElementById('suboperator1').innerHTML="Assignment substraction Operator";
+document.getElementById('ans1').innerHTML="<code>let Substr= 20</code> <br/> <code> Substr -= 4</code><br/> <code> Substr -= 5</code><br/> Answer:"  +  substr;
+
+document.getElementById('suboperator2').innerHTML="Assignment modulus Operator";
+document.getElementById('ans2').innerHTML="<code>let Modul= 25</code> <br/> <code> Modul %= 4</code><br/> <code> Modul %= 3</code><br/> Answer:"  +  modul;
+
+document.getElementById('suboperator3').innerHTML="Assignment division Operator";
+document.getElementById('ans3').innerHTML="<code>let Divide= 20</code> <br/> <code> Divide /= 4</code><br/> <code> Divide /= 4</code><br/> Answer:"  +  divide;
+
+document.getElementById('suboperator4').innerHTML="Assignment multiplication Operator";
+document.getElementById('ans4').innerHTML="<code>let Multiply= 6</code> <br/> <code> Multiply *= 4</code><br/> <code> Multiply *= 5</code><br/> Answer:"  +  multiply;
+
+document.getElementById('suboperator5').innerHTML="Assignment exponential Operator";
+document.getElementById('ans5').innerHTML="<code>let Expo= 2</code> <br/> <code> Expo **= 2</code><br/> <code> Expo **= 3</code><br/> Answer:"  +  expo;
+
+document.getElementById('headingg').innerHTML="My Third Experience with Javascript"
+document.getElementById('experience3').innerHTML="I learnt another topic in javascript today, Operators. There are six JS operators: arithmetric, assignment, comparison, logical, conditional and bitwise operators. We started with the arithmetic operator; these operators are used to perform arithmetic on numbers: +addition, -subtraction, %modulus - gives only remainder, *multiplication, **exponential - raise to power & /division"
+document.getElementById('break').innerHTML="<b>NB:</b> (+) can be used for concatenation i.e. adding two strings together<br> e.g. <code>let Firstname = \"Sarah\"</code> <br/> <code> let lastname = \"Oluwasami\"</code><br/> <code> let fullname = firstname + lastname</code><br/> Answer: Sarah Oluwasami"
+document.getElementById('heading1').innerHTML="<i>Assignment</i>"
+document.getElementById('heading2').innerHTML="Assignment Operator"
+document.getElementById('content').innerHTML="Assignment operators assign values to JavaScript variables."
+document.getElementById('heading3').innerHTML="Types of Assignment Operator"
+
+
+
+document.getElementById('heading').innerHTML="My First Experience with Javascript "
+document.getElementById('experience').innerHTML="I'm learning javascript out of neccessity to add dynamic behavior to websites and because it is the next on my front-web class outline. The first class seemed simple since it was just introduction to javascript. I've heard that javascript is hard, but I'm hoping I will understand it with practice.  From the first class, I realized that javascript is hard and also not hard, and as my teacher said 'You just need to make more effort to practice.'"
