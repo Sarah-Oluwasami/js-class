c91c96b78720d3eabe893a5668ceaa8eb9be2d8b
Sarah Oluwasami <saraholuwasami43@gmail.com>
2022-10-01 02:46:18 +0100

    day-6

---
+++ sixth-experience.js
@@ -0,0 +1,87 @@
+
+function seven() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 7
+} 
+
+function eight() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 8
+} 
+
+function nine() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 9
+} 
+
+function times() {
+    let output = document.getElementById('nine');
+        output.innerHTML += "*"
+} 
+
+function four() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 4
+} 
+
+function five() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 5
+} 
+
+function six() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 6
+} 
+
+function minus() {
+    let output = document.getElementById('nine');
+        output.innerHTML += "-"
+} 
+
+function one() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 1
+} 
+
+function two() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 2
+} 
+
+function three() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 3
+} 
+
+function plus() {
+    let output = document.getElementById('nine');
+        output.innerHTML += "+"
+} 
+
+function modulus() {
+    let output = document.getElementById('nine');
+        output.innerHTML += "%"
+} 
+
+function zero() {
+    let output = document.getElementById('nine');
+        output.innerHTML += 0
+} 
+
+function divide() {
+    let output = document.getElementById('nine');
+        output.innerHTML += "/"
+} 
+
+function equals() {
+    let output = document.getElementById('nine');
+    let result = eval(output.innerHTML);
+        output.innerHTML=result
+} 
+ 
+
+
+document.getElementById('heading').innerHTML="My Sixth Experience with Javascript"
+document.getElementById('heading1').innerHTML="<i>Assignment</i>"
+document.getElementById('heading2').innerHTML="JavaScript Form"
