6ac119cffab704ce4663045bbb3aaa4dd5395eb4
Sarah Oluwasami <saraholuwasami43@gmail.com>
2022-10-18 12:47:11 +0100

    practice

--- practice.js
+++ practice.js
@@ -1,21 +1,38 @@
 
-        let scores = [40,15,77,80,39,35];
+let scores = [40,15,77,80,39,35];
 
-        for(let m = 0; m < scores.length; m++){
-            if(scores[m] >= 50){
-                output.innerHTML += scores[m] + "passed<br>";
-                
-            }
-                
-                else if (scores[m] + 10 >= 50){
-                        output.innerHTML += scores[m] +  "You failed in your previous score but passed after adding 10marks" + " " + (scores[m] + 10)  + "<br>"
-                }
+for(let m = 0; m < scores.length; m++){
+if(scores[m] >= 50){
+    output.innerHTML += scores[m] + "passed<br>";
+    
+}
+    
+    else if (scores[m] + 10 >= 50){
+            output.innerHTML += scores[m] +  "You failed in your previous score but passed after adding 10marks" + " " + (scores[m] + 10)  + "<br>"
+    }
 
-                else{
-                    output.innerHTML += scores[m] + 10 + "You failed in your previous score but failed even after adding 10marks" + " " + scores[m] + "<br>"
-                }
-                }
-                    
+    else{
+        output.innerHTML += scores[m] + 10 + "You failed in your previous score but failed even after adding 10marks" + " " + scores[m] + "<br>"
+    }
+    }
 
+// Concat
+function concat() {
+    var surName = ["Oluwasami"];
+    var name = ["Sarah", "Tobiloba", "Ayoola"];
+    var fullName = surName.concat(name); 
+    document.getElementById("concat").innerHTML = fullName;
+  }
+        
+// // findIndex
+//  var ages = [3, 10, 18, 20];
+
+// function checkAdult(age) {
+//     return age >= 18;
+//   }
+  
+//   function findIndex() {
+//     document.getElementById("findindex").innerHTML = ages.findIndex(checkAdult);
+//   }
        
    
